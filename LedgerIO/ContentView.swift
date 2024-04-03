abcdae2a6f6ca5bd17641233f82098476076ce85
Alex Lazcano <alazcano2000@gmail.com>
2024-04-03 13:13:31 -0700

    Initial Commit

---
+++ LedgerIO/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LedgerIO
+//
+//  Created by Alex Lazcano on 2024-04-03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LedgerIO/LedgerIOApp.swift
@@ -0,0 +1,17 @@
+//
+//  LedgerIOApp.swift
+//  LedgerIO
+//
+//  Created by Alex Lazcano on 2024-04-03.
+//
+
+import SwiftUI
+
+@main
+struct LedgerIOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
